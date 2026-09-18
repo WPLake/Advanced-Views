dda12d9c06b24d6c15a43b6da2365a2b8540e79d
Maxim Akimov <lightsourcee@pm.me>
2026-09-18 13:59:36 +0300

    v3.9.7/elementor-integration - structure

--- src/Field_Provider/Core/.field_provider.domain.php
+++ src/Field_Provider/Core/.field_provider.domain.php
@@ -3,10 +3,16 @@
 namespace Org\Wplake\Advanced_Views\Field_Provider\Core;
 
 use Architecture\Policy\Domain_Policy;
+use Org\Wplake\Advanced_Views\Post_Type\Core\post_type_domain;
+use Org\Wplake\Advanced_Views\Post_Type\Types\post_types_domain;
 use Org\Wplake\Advanced_Views\Template_Engine\Core\template_engine_domain;
 
 class field_provider_domain extends Domain_Policy {
 	const WHITELIST_DOMAINS = [
 		template_engine_domain::class,
+		// todo break cycle-dependency
+		post_type_domain::class,
+		// todo break concrete dependency.
+		post_types_domain::class,
 	];
 }
